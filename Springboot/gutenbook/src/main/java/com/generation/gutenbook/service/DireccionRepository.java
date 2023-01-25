9e6f75370828e94662c9e90c9245f6c442cdb43b
54mur41mx <c.albert15va@gmail.com>
2023-01-25 17:16:40 -0600

    Se sube avance de Spring, falta solucion definitiva

---
+++ Springboot/gutenbook/src/main/java/com/generation/gutenbook/service/DireccionRepository.java
@@ -0,0 +1,9 @@
+package com.generation.gutenbook.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gutenbook.model.Direccion;
+
+public interface DireccionRepository extends JpaRepository<Direccion, Integer>{
+
+}
